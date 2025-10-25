cf23e22ae0d721247120749769c7d6bc09ccfb7f
Phúc Dev <phucbuitrong.dev@gmail.com>
2025-10-25 10:39:42 +0700

    del 2 module

--- src/subscription-sellers/dto/subscription-seller.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
-
-export class SubscriptionSellerDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  id: string;
-}
--- src/subscription-sellers/dto/update-subscription-seller.dto.ts
+++
@@ -1,9 +0,0 @@
-// Don't forget to use the class-validator decorators in the DTO properties.
-// import { Allow } from 'class-validator';
-
-import { PartialType } from '@nestjs/swagger';
-import { CreateSubscriptionSellerDto } from './create-subscription-seller.dto';
-
-export class UpdateSubscriptionSellerDto extends PartialType(
-  CreateSubscriptionSellerDto,
-) {}
